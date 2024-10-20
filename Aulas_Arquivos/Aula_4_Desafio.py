277f4465ab0912a920e8c5e4e1880cbda3014d02
charlesanderson25 <charles_spfc@hotmail.com>
2024-10-20 10:57:17 -0300

    desafio aula 4

---
+++ Aulas_Arquivos/Aula_4_Desafio.py
@@ -0,0 +1,17 @@
+name = input('Qual seu nome?')
+print('Seja bem vindo(a):', name)
+
+dt_nascimento = input ('Informe a data completa do seu nascimento!')
+print('A data de seu nascimento é:', dt_nascimento)
+
+dia = input('Informe o dia do seu nascimento')
+mes = input('Informe o mes do seu nascimento')
+ano = input('Informe o ano do seu nascimento')
+print('Você nasceu em', dia, 'de', mes, 'de', ano, '. Correto?')
+
+num1 = input('Digite o primeiro número:')
+num2 = input('Digite o segundo número:')
+soma = (num1 + num2)
+print(soma)
+
+
