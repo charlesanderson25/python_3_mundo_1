f1f5af4684eb1c5e2eef4d722392001e1f326ca1
charlesanderson25 <charles_spfc@hotmail.com>
2024-10-20 10:52:52 -0300

    Media

---
+++ ex008.py
@@ -0,0 +1,8 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a média
+
+nota1 = float(input('Informe sua primeira nota:'))
+nota2 = float(input('Informe sua segunda nota:'))
+
+media = (nota1 + nota2) / 2
+
+print('Sua média de notas é {}'.format(media))
