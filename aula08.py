088cfdd3294997f567cd7793dfc5981a80e95bbe
charlesanderson25 <charles_spfc@hotmail.com>
2024-10-20 10:57:36 -0300

    aula 8, random, math e emoji

---
+++ aula08.py
@@ -0,0 +1,14 @@
+import math
+import random
+import emoji
+
+num = random.randint(1, 10)
+
+print('O número gerado pela Random é: ', num)
+
+raizQuadrada = math.sqrt(num)
+
+print('A raiz quadrada do número gerado é: {:.2f} :thumbsup: '.format(raizQuadrada))
+
+print(emoji.emojize('Olá emojis 🤡'))
+
