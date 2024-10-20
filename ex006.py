2a3145c15487444f99247687e55a93da52e602ec
charlesanderson25 <charles_spfc@hotmail.com>
2024-10-20 10:52:24 -0300

    Antecessor e sucessor

---
+++ ex006.py
@@ -0,0 +1,8 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e antecessor
+
+n = int(input('Informe um número:'))
+s = n + 1
+a = n - 1
+
+print('Analisando o valor {}, \n O sucessor é: {} \n e o antecessor é: {}'.format(n, s, a))
+
