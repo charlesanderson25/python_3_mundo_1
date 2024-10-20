7b257e09ab034424830a155f23488b4519e2125e
charlesanderson25 <charles_spfc@hotmail.com>
2024-10-20 10:54:44 -0300

    tabuada

---
+++ ex010.py
@@ -0,0 +1,36 @@
+# Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada
+
+n = int(input('Informe um número inteiro'))
+
+t1 = n * 1
+t2 = n * 2
+t3 = n * 3
+t4 = n * 4
+t5 = n * 5
+t6 = n * 6
+t7 = n * 7
+t8 = n * 8
+t9 = n * 9
+t10 = n * 10
+
+print('O primeiro valor da tabuada é: {}'.format(t1))
+print('O segundo valor da tabuada é: {}'.format(t2))
+print('O terceiro valor da tabuada é: {}'.format(t3))
+print('O quarto valor da tabuada é: {}'.format(t4))
+print('O quinto valor da tabuada é: {}'.format(t5))
+print('O sexto valor da tabuada é: {}'.format(t6))
+print('O sétimo valor da tabuada é: {}'.format(t7))
+print('O oitavo valor da tabuada é: {}'.format(t8))
+print('O nono valor da tabuada é: {}'.format(t9))
+print('O décimo valor da tabuada é: {}'.format(t10))
+
+print('{} x 1  = {}'.format(n, t1))
+print('{} x 2  = {}'.format(n, t2))
+print('{} x 3  = {}'.format(n, t3))
+print('{} x 4  = {}'.format(n, t4))
+print('{} x 5  = {}'.format(n, t5))
+print('{} x 6  = {}'.format(n, t6))
+print('{} x 7  = {}'.format(n, t7))
+print('{} x 8  = {}'.format(n, t8))
+print('{} x 9  = {}'.format(n, t9))
+print('{} x 10 = {}'.format(n, t10))
