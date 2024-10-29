6f8bf16eed35e150c80073ff550da990906b0fb7
charlesanderson25 <charles_spfc@hotmail.com>
2024-10-29 18:35:52 -0300

    Add time lib

--- ex030.py
+++ ex030.py
@@ -1,10 +1,14 @@
 #Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
 
 import random
+import time
 
 nrUsuario = int(input('Informe um número inteiro entre 0 e 5:'))
 
 numeroPC = random.randint(1, 5)
+
+print('Processando...')
+time.sleep(3)
 
 print('O número sorteado foi: {}'.format(numeroPC))
 
