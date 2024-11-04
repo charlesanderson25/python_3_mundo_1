c3a6bf8732960b207175094d14228e8fd8bc3eba
charlesanderson25 <charles_spfc@hotmail.com>
2024-11-04 10:42:12 -0300

    teste_mundo1

---
+++ teste_mundo1.py
@@ -0,0 +1,9 @@
+#Considere a string s = 'prova de python'. A instrução len(s) retornaria qual valor?
+
+s = 'prova de python'
+
+print(len(s))
+
+x = 'curso de python no cursoemvideo'
+
+print(x[:5])
